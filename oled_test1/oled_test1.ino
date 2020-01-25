080c16655cc63bd17b3c976961fdc7e55f06f97f
Jack Cox <jcox@captechconsulting.com>
2020-01-25 13:08:01 -0500

    initial version that displays hello world and turns off the display when it's a little dark

---
+++ oled_test1/oled_test1.ino
@@ -0,0 +1,109 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeMono12pt7b.h>
+
+#define SCREEN_WIDTH  128
+#define SCREEN_HEIGHT 64
+
+#define OLED_RESET  4
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+#define WHITE SSD1306_WHITE
+#define BLACK SSD1306_BLACK
+
+bool blinkState = false;
+int loopCount = 0;
+
+const int prPin = 0; // photo resistor pin in A0
+const int lightThreshold = 50;
+
+int lightVal;
+bool displayOn;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while(!Serial); // wait for serial to init
+ 
+  
+  Serial.println("Starting setup");
+  
+  delay(200);
+  
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  // init done
+  displayOn = true;
+  
+  //display.display(); // show splashscreen
+  //delay(2000);
+  display.clearDisplay();   // clears the screen and buffer
+
+  // draw a single pixel
+  display.drawPixel(10, 10, WHITE);
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
+  display.setFont(&FreeMono12pt7b);
+
+}
+
+void testdrawcircle(void) {
+  display.clearDisplay();
+
+  for(int16_t i=0; i<max(display.width(),display.height())/2; i+=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
+    display.display();
+    delay(1);
+  }
+
+  delay(2000);
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //testdrawcircle();
+
+  lightVal = analogRead(prPin);
+  display.clearDisplay();
+  
+  if (lightVal > lightThreshold) {
+    if (!displayOn) { // turn on the display
+      display.ssd1306_command(SSD1306_DISPLAYON);
+      displayOn = true; 
+    }
+
+    display.setCursor(0,14);
+    display.setTextSize(1);
+    display.setTextWrap(true);
+    display.setTextColor(WHITE, BLACK);
+    display.print("hello world! ");
+    display.print(lightVal);
+    
+  
+    display.drawPixel(display.width()-1, display.height()-1, (blinkState ? WHITE : BLACK));
+    blinkState = (blinkState ? false : true);
+    
+    
+    Serial.print("printed text, light value= ");
+    Serial.println(lightVal);
+  } else {
+      if (displayOn) { // turn on the display
+        display.ssd1306_command(SSD1306_DISPLAYOFF);
+        displayOn = false; 
+      }
+  }
+  display.display();
+  delay(500);
+
+}
